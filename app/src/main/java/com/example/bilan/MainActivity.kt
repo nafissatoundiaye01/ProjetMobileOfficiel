e24308e3a5484da216e9f0377befcdaacf8b7ac5
Nafissatou NDIAYE <nafissatound1@gmail.com>
2023-05-02 11:50:17 +0000

    Initial commit

--- app/src/main/java/com/example/bilan/MainActivity.kt
+++ app/src/main/java/com/example/bilan/MainActivity.kt
@@ -8,33 +8,13 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModel
 
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import androidx.navigation.NavigatorProvider
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.bilan.database.FitnessDatabase
-import com.example.bilan.models.User
-import com.example.bilan.repositories.UserRepository
 import com.example.bilan.ui.theme.BilanTheme
-import com.example.bilan.viewmodels.AjoutViewModel
-import com.example.bilan.viewmodels.BilanViewModel
 import com.example.bilan.viewmodels.Graph
-import com.example.bilan.viewmodels.UserViewModel
 import com.example.bilan.views.*
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/bilan/views/MyView.kt
+++ app/src/main/java/com/example/bilan/views/MyView.kt
@@ -2,38 +2,27 @@ package com.example.bilan.views
 
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Horizontal
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -41,15 +30,9 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.bilan.interfaces.UserDao
 import com.example.bilan.models.User
-import com.example.bilan.viewmodels.AjoutViewModel
 import com.example.bilan.viewmodels.BilanViewModel
-import com.example.bilan.viewmodels.UserViewModel
 
 
 @SuppressLint("SuspiciousIndentation")
