92140a1010279f5a10e9f82201e568fdc08d2ee9
Nafissatou NDIAYE <nafissatound1@gmail.com>
2023-05-02 12:17:44 +0000

    Initial commit

--- app/src/main/java/com/example/bilan/database/FitnessDatabase.kt
+++ app/src/main/java/com/example/bilan/database/FitnessDatabase.kt
@@ -6,10 +6,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.bilan.interfaces.UserDao
+import com.example.bilan.models.Entrainements
 import com.example.bilan.models.User
 
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class,Entrainements::class], version = 1, exportSchema = false)
 abstract class FitnessDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
