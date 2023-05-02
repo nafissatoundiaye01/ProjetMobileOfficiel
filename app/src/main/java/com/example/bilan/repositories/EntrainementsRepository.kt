e2b41fdedb5d22c09b129a2a133481820b735e3e
Nafissatou NDIAYE <nafissatound1@gmail.com>
2023-05-02 13:13:24 +0000

    Initial commit

--- app/src/main/java/com/example/bilan/repositories/EntrainementsRepository.kt
+++ app/src/main/java/com/example/bilan/repositories/EntrainementsRepository.kt
@@ -1,5 +1,42 @@
 package com.example.bilan.repositories
 
-class EntrainementsRepository {
+
+import com.example.bilan.interfaces.EntrainementsDao
+import com.example.bilan.models.Entrainements
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+
+class EntrainementsRepository(private val entrainementsDao: EntrainementsDao) {
+
+
+    suspend fun insertEntrainements(todo: Entrainements) {
+        Dispatchers.IO.apply {
+            entrainementsDao.insertEntrainements(todo)
+        }
+    }
+
+    suspend fun deleteEntrainements(todo: Entrainements) {
+        Dispatchers.IO.apply {
+            entrainementsDao.deleteEntrainements(todo)
+        }
+    }
+
+    suspend fun updateEntrainements(todo:Entrainements) {
+        Dispatchers.IO.apply {
+            entrainementsDao.updateEntrainements(todo)
+        }
+    }
+
+    suspend fun getAllEntrainementsWeek(id:Int): Flow<List<Entrainements>>? {
+        Dispatchers.IO.apply {
+            return entrainementsDao.getAllEntrainementsWeek(id)
+        }
+    }
+
+    suspend fun getEntrainementsByDate(id: Int, dayOfWeek: String): Entrainements? {
+        Dispatchers.IO.apply {
+            return entrainementsDao.getEntrainementsByDate(id, dayOfWeek)
+        }
+    }
 
 }
